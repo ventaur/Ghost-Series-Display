6d388f0955cbe0a6ff249b0ca0dca0b6f24a41b5
Matt Sollars <ventaur@gmail.com>
2022-01-02 01:37:53 -0500

    Refactor test scenarios to their own module

--- test/series-display.test.js
+++ test/series-display.test.js
@@ -1,42 +1,10 @@
-import Chance from 'chance';
-const chance = new Chance();
-
-import cloneDeep from 'lodash.clonedeep';
-import { loadJsonFile } from 'load-json-file';
 import should from 'chai';
 
-import SeriesDisplay from '../lib/index.js';
-
-
-const SeriesTagDecDaily = 'series-december-daily-2021';
-const SeriesTagFluffy = 'series-fluffy-2021';
-const TagsBySlug = await loadJsonFile('./test/test-tags.json');
-const TestPosts = await loadJsonFile('./test/test-posts.json');
+import { SeriesTagDecDaily, SeriesTagFluffy, createSeriesDisplayWithDecDailyAndFluffy } from './testScenarios.js';
 
 
 describe('Series Display', function () {
     let seriesDisplayWithDecDailyAndFluffy;
-
-
-    function addTagToPosts(tagSlug, posts, postIndeces) {
-        const tag = TagsBySlug[tagSlug];
-        for (const postIndex of postIndeces) {
-            posts[postIndex].tags.push(tag);
-        }
-    }
-
-    function createApi(posts) {
-        return { posts: { browse: async () => ({ posts }) } };
-    }
-
-    function createSeriesDisplayWithDecDailyAndFluffy() {
-        const posts = cloneDeep(TestPosts);
-        addTagToPosts(SeriesTagDecDaily, posts, [0, 1, 2, 3, 10]);
-        addTagToPosts(SeriesTagFluffy, posts, [4, 5, 6, 7, 8, 9, 10, 11]);
-        
-        const api = createApi(posts);
-        return new SeriesDisplay(api);
-    }    
 
     before(function () {
         seriesDisplayWithDecDailyAndFluffy = createSeriesDisplayWithDecDailyAndFluffy();
@@ -45,7 +13,6 @@ describe('Series Display', function () {
 
     it('stub', async function () {
         const html = await seriesDisplayWithDecDailyAndFluffy.getSeriesInfoHtml([ SeriesTagDecDaily, SeriesTagFluffy ]);
-        console.log(TestPosts[10]);
-        console.log(html[10]);
+        return true;
     });
 });
---
+++ test/testScenarios.js
@@ -0,0 +1,33 @@
+import cloneDeep from 'lodash.clonedeep';
+import { loadJsonFile } from 'load-json-file';
+
+import SeriesDisplay from '../lib/index.js';
+
+
+export const SeriesTagDecDaily = 'series-december-daily-2021';
+export const SeriesTagFluffy = 'series-fluffy-2021';
+
+const TagsBySlug = await loadJsonFile('./test/test-tags.json');
+const TestPosts = await loadJsonFile('./test/test-posts.json');
+
+
+function addTagToPosts(tagSlug, posts, postIndeces) {
+    const tag = TagsBySlug[tagSlug];
+    for (const postIndex of postIndeces) {
+        posts[postIndex].tags.push(tag);
+    }
+}
+
+function createApi(posts) {
+    return { posts: { browse: async () => ({ posts }) } };
+}
+
+
+export function createSeriesDisplayWithDecDailyAndFluffy() {
+    const posts = cloneDeep(TestPosts);
+    addTagToPosts(SeriesTagDecDaily, posts, [0, 1, 2, 3, 10]);
+    addTagToPosts(SeriesTagFluffy, posts, [4, 5, 6, 7, 8, 9, 10, 11]);
+    
+    const api = createApi(posts);
+    return new SeriesDisplay(api);
+}
