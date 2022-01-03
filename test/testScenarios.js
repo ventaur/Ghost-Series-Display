97a12dde58f12aab0c60f046dffacead3938a18d
Matt Sollars <ventaur@gmail.com>
2022-01-03 17:24:05 -0500

    Add new scenario for posts in Fluffy tag

--- test/testScenarios.js
+++ test/testScenarios.js
@@ -8,11 +8,21 @@ export const SeriesTagDecDaily = 'series-december-daily-2021';
 export const SeriesTagFluffy = 'series-fluffy-2021';
 
 const TagsBySlug = await loadJsonFile('./test/test-tags.json');
-const TestPosts = await loadJsonFile('./test/test-posts.json');
+const TestPosts = await loadJsonFile('./test/test-posts.json'); // 12 posts
 
 
 function addTagToPosts(tagSlug, posts, postIndeces) {
     const tag = TagsBySlug[tagSlug];
+
+    // Tag all posts.
+    if (postIndeces === undefined) {
+        posts.forEach(post => {
+            post.tags.push(tag);
+        });
+        return;
+    }
+
+    // Tag just the posts with indeces specified.
     for (const postIndex of postIndeces) {
         posts[postIndex].tags.push(tag);
     }
@@ -23,11 +33,28 @@ function createApi(posts) {
 }
 
 
-export function createSeriesDisplayWithDecDailyAndFluffy() {
+export function createSeriesDisplayWithFluffyPosts() {
+    // 8 posts
+    const fluffyPosts = TestPosts.slice(4);
+    const posts = cloneDeep(fluffyPosts);
+    addTagToPosts(SeriesTagFluffy, posts);
+    
+    const api = createApi(posts);
+    return {
+        seriesDisplay: new SeriesDisplay(api),
+        posts
+    };
+}
+
+export function createSeriesDisplayWithDecDailyAndFluffyPosts() {
+    // 12 posts
     const posts = cloneDeep(TestPosts);
     addTagToPosts(SeriesTagDecDaily, posts, [0, 1, 2, 3, 10]);
     addTagToPosts(SeriesTagFluffy, posts, [4, 5, 6, 7, 8, 9, 10, 11]);
     
     const api = createApi(posts);
-    return new SeriesDisplay(api);
+    return {
+        seriesDisplay: new SeriesDisplay(api),
+        posts
+    };
 }
